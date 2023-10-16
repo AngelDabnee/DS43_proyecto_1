6dc122cdad84a23b815be849f22cdbb6771a3351
AngelDabnee <a22311046@uthermosillo.edu.mx>
2023-10-16 15:52:52 -0700

    layout complet
    Signed-off-by: AngelDabnee <a22311046@uthermosillo.edu.mx>

--- app/src/main/java/AngelDabnee/convertidordegrados/MainActivity.kt
+++ app/src/main/java/AngelDabnee/convertidordegrados/MainActivity.kt
@@ -2,10 +2,28 @@ package AngelDabnee.convertidordegrados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val captureTemperautre = findViewById<EditText>(R.id.editTextNumberDecimal_write_temperature)
+        val selectConvert = findViewById<Spinner>(R.id.spinner)
+        val bottonConvert = findViewById<Button>(R.id.button_convert)
+        val showResult = findViewById<TextView>(R.id.textView_results)
+
+        bottonConvert.setOnClickListener{
+            Toast.makeText(this,"" + captureTemperautre.text, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    public fun converter(){
+        //TODO add when statement to process conversion.
+
     }
 }
---
+++ app/src/main/java/AngelDabnee/convertidordegrados/models/Temperature.kt
@@ -0,0 +1,7 @@
+package AngelDabnee.convertidordegrados.models
+
+data class Temperature(
+    val temp: Double,
+    val origin: Char,
+    val destiny: Char
+)
